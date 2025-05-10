8ef7c4a545e7dcaed03ae467b0afcd2b223828d9
tam1444AH <mohammedtalha290@gmail.com>
2025-05-09 20:34:51 -0500

    added signup form func

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css'
 })
 export class SellerAuthComponent {
-
+  signUp(data: object): void {
+    console.warn(data);
+  }
 }
