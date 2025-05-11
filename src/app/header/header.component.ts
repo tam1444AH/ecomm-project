342fed8e2665cee656922f975b19ae9a19b577da
tam1444AH <mohammedtalha290@gmail.com>
2025-05-10 19:29:14 -0500

    added dynamic navbar and logout and routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import  { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuType: string = 'default';
+  sellerName: string = '';
+  constructor(private route: Router) { }
+  ngOnInit() {
+    this.route.events.subscribe((val: any) => {
+    
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          let sellerStore = localStorage.getItem('seller');
+          let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+          this.menuType = 'seller';
+          this.sellerName = sellerData.name;
+        }
+        else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 
+  logout() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
 }
---
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-add-product',
+  imports: [],
+  templateUrl: './seller-add-product.component.html',
+  styleUrl: './seller-add-product.component.css'
+})
+export class SellerAddProductComponent {
+
+}
