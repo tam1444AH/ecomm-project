4285b21e44dceb88ed96d9ea2b3512dbdc44ee51
tam1444AH <mohammedtalha290@gmail.com>
2025-05-12 10:35:44 -0500

    completed product update form

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -9,24 +9,35 @@ import { Product } from '../data-type';
   imports: [FormsModule],
   standalone: true,
   templateUrl: './seller-update-product.component.html',
-  styleUrl: './seller-update-product.component.css'
+  styleUrl: './seller-update-product.component.css',
 })
 export class SellerUpdateProductComponent {
   productData: undefined | Product;
+  productMessage: undefined | string;
   constructor(private route: ActivatedRoute, private product: ProductService) {}
-  
+
   ngOnInit() {
     let productId = this.route.snapshot.paramMap.get('id');
     if (productId) {
       this.product.getProduct(productId).subscribe((result) => {
         console.warn(result);
         this.productData = result;
-      })
+      });
     }
   }
 
-
-  submit(data: any) {
-    console.warn(data);
+  submit(data: Product) {
+    if (this.productData) {
+      data.id = this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+      if (result) {
+        console.warn(result);
+        this.productMessage = 'Product updated successfully';
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
   }
 }
