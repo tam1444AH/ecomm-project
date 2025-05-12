0f8eb27ef178d814af5a91f0675e58c476da2a2e
tam1444AH <mohammedtalha290@gmail.com>
2025-05-12 13:06:36 -0500

    display product in caresuel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { Product } from '../data-type';
+import { ProductService } from '../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgbCarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  popularProducts: Product[] | undefined;
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor(private product: ProductService) { }
 
+  ngOnInit() {
+    this.product.popularProducts().subscribe((result) => {
+      if (result) {
+        this.popularProducts = result;
+      }
+    });
+  }
 }
