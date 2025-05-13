38a9a7d6dc8e78f4c307f77c5271787714361705
tam1444AH <mohammedtalha290@gmail.com>
2025-05-13 10:27:24 -0500

    added dynamic search for products page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../data-type';
+import { ProductService } from '../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-
+  searchResult: Product[] | undefined;
+  constructor(private activeRoute: ActivatedRoute, private product: ProductService) {}
+  ngOnInit() {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    if (query) {
+      this.product.searchProducts(query).subscribe((result) => {
+        if (result.length > 0) {
+          this.searchResult = result;
+        }
+        else {
+          this.searchResult = undefined;
+        }
+      });
+    }
+  }
 }
